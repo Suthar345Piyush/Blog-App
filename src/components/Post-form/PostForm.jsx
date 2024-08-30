235d721eb35a133b4f98302f7c8bd91165f76331
Piyush Suthar <Piyushsuthar524@gmail.com>
2024-08-31 01:01:12 +0530

    Update PostForm.jsx

--- src/components/Post-form/PostForm.jsx
+++ src/components/Post-form/PostForm.jsx
@@ -54,15 +54,17 @@ function PostForm({post}) {
     }
   };
 
+  // slugTransform method
+
   const slugTransform = useCallback((value) => {
     if(value && typeof value === 'string')
       return value 
       .trim()
       .toLowerCase()
-      .replace(/[^a-zA-Z\d\s]+/g, "-")
-      .replace(/\s/g, "-");
+      .replace(/[^a-zA-Z\d\s]+/g, '-') //replace() method main koi bhi pattern match kar skte hain, regex is used , ^ is neggets.
+      .replace(/\s/g, '-');
 
-      return "";
+      return ''
     
     }, []);
 
@@ -77,9 +79,55 @@ function PostForm({post}) {
     }, [watch, slugTransform, setValue]);
     })
 
-  return (
-    <div>post form </div>
-  )
-  }
+ return (
+    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+        <div className="w-2/3 px-2">
+            <Input
+                label="Title :"
+                placeholder="Title"
+                className="mb-4"
+                {...register("title", { required: true })}
+            />
+            <Input
+                label="Slug :"
+                placeholder="Slug"
+                className="mb-4"
+                {...register("slug", { required: true })}
+                onInput={(e) => {
+                    setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
+                }}
+            />
+            <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
+        </div>
+        <div className="w-1/3 px-2">
+            <Input
+                label="Featured Image :"
+                type="file"
+                className="mb-4"
+                accept="image/png, image/jpg, image/jpeg, image/gif"
+                {...register("image", { required: !post })}
+            />
+            {post && (
+                <div className="w-full mb-4">
+                    <img
+                        src={appwriteService.getFilePreview(post.featuredImage)}
+                        alt={post.title}
+                        className="rounded-lg"
+                    />
+                </div>
+            )}
+            <Select
+                options={["active", "inactive"]}
+                label="Status"
+                className="mb-4"
+                {...register("status", { required: true })}
+            />
+            <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+                {post ? "Update" : "Submit"}
+            </Button>
+        </div>
+    </form>
+);
+
 
 export default PostForm
